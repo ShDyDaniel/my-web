e150221469ff3b6e1b2a8d9da61f2e327f61c820
pizzabossxd <68411735+pizzabossxd@users.noreply.github.com>
2024-05-16 20:22:16 +0300

    Update Landing.tsx

--- src/uicomponents/Landing.tsx
+++ src/uicomponents/Landing.tsx
@@ -44,10 +44,10 @@ export default function Landing() {
             <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
               <div>
                 <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                  Daniel Shalts
+                  דניאל שלץ
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-200 md:text-xl">
-                  I'm a videographer and editor with experience in the field.
+                  צלם ועורך וידאו מקצועי
                 </p>
                 <div className="space-x-4 mt-6">
                   <Link
