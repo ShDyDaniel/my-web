b2cb9982a739d84e88842877b55d5265a86e7a50
ShDyDaniel <dyshalts@gmail.com>
2024-05-16 20:06:58 +0300

    Update Landing.tsx

--- src/uicomponents/Landing.tsx
+++ src/uicomponents/Landing.tsx
@@ -11,7 +11,7 @@ export default function Landing() {
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
           <UserIcon className="h-6 w-6" />
-          <span className="sr-only">דניאל שלץ</span>
+          <span className="sr-only">Daniel Shalts</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6 scroll-smooth">
           <Link
@@ -44,7 +44,7 @@ export default function Landing() {
             <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
               <div>
                 <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]">
-                  דניאל שלץ
+                  Daniel Shalts
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-200 md:text-xl">
                   I'm a videographer and editor with experience in the field.
