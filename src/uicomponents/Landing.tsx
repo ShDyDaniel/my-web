df0a1fc4ac7f73d431eabe6ca953567fc698f8b9
pizzabossxd <68411735+pizzabossxd@users.noreply.github.com>
2024-05-27 23:45:33 +0300

    Update Landing.tsx

--- src/uicomponents/Landing.tsx
+++ src/uicomponents/Landing.tsx
@@ -41,12 +41,12 @@ export default function Landing() {
           style={{ backgroundImage: "url(/assets/videography.jpg)" }}
         >
           <div className="px-4 md:px-6 space-y-10 xl:space-y-16 bg-black/50 backdrop-blur-sm py-12 md:py-24 lg:py-64 rounded-md">
-            <div className="grid max-w-[1300px] mx-auto gap-4 px-4 sm:px-6 md:px-10 md:grid-cols-2 md:gap-16">
+            <div className="px-4 sm:px-6 md:px-10">
               <div>
                 <h1 className="lg:leading-tighter text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl xl:text-[3.4rem] 2xl:text-[3.75rem]" style={{ direction: "rtl" }}>
                   דניאל שלץ
                 </h1>
-                <p className="mx-auto max-w-[700px] text-gray-200 md:text-xl" style={{ direction: "rtl" }}>
+                <p className="mx-auto text-gray-200 md:text-xl" style={{ direction: "rtl" }}>
                   צלם ועורך וידאו מקצועי
                 </p>
                 <div className="space-x-4 mt-6" style={{ direction: "rtl" }}>
